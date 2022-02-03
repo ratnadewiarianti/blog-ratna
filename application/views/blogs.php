379ced2ad06414154dda208d54a8938687d3075c
ratnadewiarianti <96292488+ratnadewiarianti@users.noreply.github.com>
2022-02-03 11:30:34 +0800

    Update blogs.php

--- application/views/blogs.php
+++ application/views/blogs.php
@@ -10,7 +10,13 @@
     <h1>Artikel Terbaru</h1>
     <h3>
     <a href="<?= site_url ('blog/add');?>"> + Tambah Artikel </a>
+    <br>
+    <form>
+        <input type="text" name="find">
+        <button type="submit">Cari</button>
+    </form>
     </h3>
+    
     <?php foreach($blogs as $key => $blog) :?>
     <div class="blog">
         <h2>
@@ -18,6 +24,8 @@
         </h2>
         <h3>
         <a href="<?= site_url ('blog/edit/'.$blog['id']);?>"> Edit </a>
+        
+        <a href="<?= site_url ('blog/delete/'.$blog['id']);?>"> Delete  </a>
         </h3>    
     <?= $blog['content']; 
      endforeach; ?>
